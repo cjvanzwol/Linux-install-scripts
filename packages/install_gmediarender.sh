ace13854b7a7aeed71b24faf01354b79fb9a78b4
Cor Jan van Zwol <cjvanzwol@gmail.com>
2020-12-24 17:30:42 +0100

    default for gmediarender

--- packages/install_gmediarender.sh
+++ packages/install_gmediarender.sh
@@ -4,17 +4,13 @@ PREFIX_=$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )
 cd $PREFIX_ && cd ..
 source ../functions.sh
 #cd $PREFIX_
+DEFAULTS=/etc/default
 
 # installing package
 echo "Installing gmrender-resurrect"
 sudo apt-get update -y
 sudo apt-get install -qq libupnp6 gmediarender
+sudo rm $DEFAULTS/gmediarender
+cp gmediarender $DEFAULTS
 sudo systemctl enable gmediarender
-echo "GMRENDER-RESURRECT DONE"-qq libupnp-dev libgstreamer1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-alsa
-git clone https://github.com/hzeller/gmrender-resurrect.git
-cd gmrender-resurrect
-./autogen.sh
-./configure
-make
-sudo make install
 echo "GMRENDER-RESURRECT DONE"
