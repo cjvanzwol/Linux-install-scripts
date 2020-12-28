63902feb690e957d4b18d470926ea32f2edb7ed6
cjvanzwol <cjvanzwol@gmail.com>
2020-12-28 17:53:31 +0000

    fix functions

--- functions.sh
+++ functions.sh
@@ -1,7 +1,12 @@
 #! /bin/sh
 if [[ $functionsSet != True ]]; then
   PREFIX_=$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )
-  echo $PREFIX_
+  #X=1
+  until [[ $PREFIX_ == *Linux-install-scripts ]]; do
+    PREFIX_=$( echo "$PREFIX_" | sed 's/.$//' )
+    #echo $X && ((X=$X+1))
+  done
+  
   ######################
   # defining functions #
   ######################
