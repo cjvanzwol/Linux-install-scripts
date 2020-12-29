acfd64d6825cad9a0706667aeef3cffb5b1d009e
cjvanzwol <cjvanzwol@gmail.com>
2020-12-29 18:55:49 +0000

    merged install_ en extend_ for jupyter

--- packages/install_jupyterhub.sh
+++ packages/install_jupyterhub.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 # DEBUG
-sudo rm /usr/bin/python /usr/bin/pip /etc/systemd/system/jupyterhub.service /usr/share/applications/jupyterhub.desktop /etc/profile.d/conda.sh
-sudo rm -r /opt/jupyterhub /opt/conda /usr/local/share/jupyter/kernels
+#sudo rm /usr/bin/python /usr/bin/pip /etc/systemd/system/jupyterhub.service /usr/share/applications/jupyterhub.desktop /etc/profile.d/conda.sh
+#sudo rm -r /opt/jupyterhub /opt/conda /usr/local/share/jupyter/kernels
+
 # preload functions
 source $( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )/packages/.recall_functions
 
@@ -92,6 +93,94 @@ read -p "Do you want to install extensions en languageservers for Jupyterlab? [y
 if [ $e != "y" ]; then
         echo "Skipping extending Jupyterlab"
 else
-        source $PREFIX_/packages/extend_jupyter.sh
+        # Installing package
+        source /opt/jupyterhub/bin/activate
+
+        echo "KERNELS"
+        echo "Installing bash_kernel"
+        sudo /opt/jupyterhub/bin/python3 -m pip install -q bash_kernel
+        sudo /opt/jupyterhub/bin/python3 -m bash_kernel.install
+
+        # #4
+        # read -p "Install Javascript? [y/N]" js 
+        # if [[ $js == "y" ]]; then
+        #     sudo npm install -g --unsafe-perm ijavascript
+        #     sudo ijsinstall --install=global
+        #fi
+
+        # #5
+        # read -p "Install Nodejs? [y/N]" nj 
+        # if [[ $nj == "y" ]]; then
+        #     source /opt/jupyterhub/bin/activate
+        #     cd sudo ~
+        #     sudo git clone https://github.com/notablemind/jupyter-nodejs.git
+        #     sudo cd jupyter-nodejs
+        #     sudo mkdir -p ~/.ipython/kernels/nodejs/
+        #     sudo npm install && sudo node install.js
+        #     sudo npm run build
+        #     sudo npm run build-ext
+        #     sudo jupyter console --kernel nodejs
+        # fi
+
+        echo "EXTENSIONS"
+        echo ">> installing"
+        sudo /opt/jupyterhub/bin/python3 -m pip install -q voila
+        jupyter labextension install -y --no-build \
+        @jupyter-widgets/jupyterlab-manager \
+        @krassowski/jupyterlab_go_to_definition \
+        @jupyterlab/google-drive \
+        @jupyterlab/toc \
+        @lckr/jupyterlab_variableinspector \
+        jupyterlab-topbar-extension jupyterlab-system-monitor jupyterlab-theme-toggle \
+        @ijmbarr/jupyterlab_spellchecker \
+        @jupyter-voila/jupyterlab-preview
+        # gave error
+        jupyter labextension install -y --no-build \
+        jupyterlab-execute-time \ # jupyterlab-execute-time: https://github.com/deshaw/jupyterlab-execute-time"
+        echo ">> CUnstimozing settings"
+        cpfile tracker.jupyterlab-settings ~/.jupyter/lab/user-settings/@jupyterlab/notebook-extension
+
+        read -p "Install Language server? [y/N]" ls
+        if [[ $ls == "y" ]]; then
+        sudo /opt/jupyterhub/bin/python3 -m pip install -q jupyter-lsp 
+        jupyter labextension install --no-build @krassowski/jupyterlab-lsp 
+        sudo /opt/jupyterhub/bin/python3 -m pip install -q python-language-server[all]
+        sudo npm install --save-dev -y \
+                bash-language-server \
+                javascript-typescript-langserver \
+                unified-language-server \
+                vscode-css-languageserver-bin \
+                vscode-html-languageserver-bin \
+                vscode-json-languageserver-bin
+        fi
+        read -p "Install Code Formatter? [y/N]" cf
+        if [[ $cf == "y" ]]; then
+        sudo /opt/jupyterhub/bin/python3 -m pip install -q jupyterlab-code-formatter 
+        jupyter labextension install -y --no-build @ryantam626/jupyterlab_code_formatter
+        sudo /opt/jupyterhub/bin/python3 -m pip install -q autopep8 #black yapf isort
+        sudo /opt/jupyterhub/bin/jupyter serverextension enable --py jupyterlab_code_formatter
+        fi
+        read -p "Install Debugger? [y/N]" db
+        if [[ $db == "y" ]]; then
+        sudo /opt/jupyterhub/bin/python3 -m pip install -q ptvsd #xeus-python #10
+        jupyter labextension install --no-build @jupyterlab/debugger
+        fi
+
+        echo "THEMES"
+        echo "dracula"
+        jupyter labextension install --no-build @telamonian/theme-darcula -y
+
+        << COMMENT
+        read -p "Install ...? [y/N]" 
+        if [[ $ == "y" ]]; then
+        sudo /opt/jupyterhub/bin/python3 -m pip install -y 
+        fi
+        COMMENT
+
+        echo "BUILDING JUPYTER LAB"
+        jupyter lab build --dev-build=False --minimize=False
+        deactivate
+        sudo systemctl restart jupyterhub
+
         echo "Jupyterlab extended"
 fi
