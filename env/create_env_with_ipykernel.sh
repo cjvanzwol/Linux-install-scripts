8b825c7f766bba3ac8a30214a93ceaaa44e69a86
Cor Jan van Zwol <cjvanzwol@gmail.com>
2021-05-17 13:56:08 +0200

    moved notebool dir to env

---
+++ env/create_env_with_ipykernel.sh
@@ -0,0 +1,48 @@
+#! /bin/sh
+# define finctions
+what_packages() {
+    read -p "What packages need to be installed on creation? [space seperated list; leave empty for None] " extra_packages
+}
+# set variables
+if [[ $(which conda) != "" ]]; then conda=true; else conda=false; fi
+
+# Installing environmentcd ..
+if [[ -f ./environment.yml ]]; then
+    envName=$(awk '/name/ {print $2}' ./environment.yml)
+    echo "Environment named '$envName' will be installed"
+    conda env create -f ./environment.yml -q
+else
+    read -p "What is the name for the enviroment? " envName
+    if [[ $(which conda) != "" ]]; then
+        read -p "Are there any other options for conda create? [leave empty for None] " options
+        conda create -n $envName pip ipykernel pandas matplotlib plotly -y -q
+        source ~/miniconda3/bin/activate $envName
+        conda config --add channels conda-forge
+        if [[ -f ./requirements.txt ]]; then
+            pip install -r requirements.txt -q
+        else
+            what_packages
+            packages="ipykernel $extra_packages"
+        fi
+        conda deactivate
+    else
+        echo "DO PIP & virtual env --> still needs to be coded"
+        ~/miniconda3/bin/python -m venv $envName
+        source ./$envName/bin/activate
+        if [[ -f ./requirements.txt ]]; then
+            python -m pip install -r requirements.txt
+        else
+            what_packages
+            python -m pip install $extra_packages
+        fi
+        python -m pip install ipykernel
+        deactivate
+    fi
+fi
+
+# Installing kernel
+if [[ $conda ]]; then
+    ~/miniconda3/envs/$envName/bin/python -m ipykernel install --name $envName --display-name $envName
+else
+    ./$envName/bin/python -m ipykernel install --name $envName --display-name $envName
+fi
