56768a27551138a60d8a6248552d16a41775d93e
Cor Jan van Zwol <cjvanzwol@gmail.com>
2021-05-27 11:24:15 +0200

    Added synology drive

---
+++ packages/install_drive.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+# preload functions
+source $(find ~ -name Linux-install-scripts 2>/dev/null)/functions.sh
+[[ $FASE == "" ]] && FASE=drive
+
+# Installing package
+echo "Installing Synology Drive"
+get https://global.download.synology.com/download/Utility/SynologyDriveClient/2.0.4-11112/Ubuntu/Installer/x86_64/synology-drive-client-11112.x86_64.deb
+echo "SYNOLOGY DRIVE DONE"
