8c527e97d655d11b6a822817fe1b675e62da7da9
speedtest <cjvanzwol@gmail.com>
2021-05-28 19:47:21 +0200

    fix creating dummy bin

--- packages/install_spotify.sh
+++ packages/install_spotify.sh
@@ -6,5 +6,5 @@ source $(find ~ -name Linux-install-scripts 2>/dev/null)/functions.sh
 # installing package
 echo "Installing spotify"
 get https://dtcooper.github.io/raspotify/raspotify-latest.deb
-echo "dummy file for Linux-install-scripts. Check https://github.com/cjvanzwol/Linux-install-scripts" > /usr/local/bin/spotify
+sudo echo "dummy file for Linux-install-scripts. Check https://github.com/cjvanzwol/Linux-install-scripts" > /usr/local/bin/spotify
 echo "spotify DONE"
