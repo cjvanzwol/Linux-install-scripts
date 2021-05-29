512e5d149dddc7e0358cb4e69132eebf666c787f
Cor Jan van Zwol <cjvanzwol@gmail.com>
2021-05-29 15:33:04 +0200

    synced repos

--- packages/install_sickrage.sh
+++ packages/install_sickrage.sh
@@ -6,11 +6,16 @@ source $(find ~ -name Linux-install-scripts 2>/dev/null)/functions.sh
 # Installing package
 echo "Installing Sickrage"
 if [[ $OS == "OSMC" ]]; then
-    #sudo apt-get install -qq git-core python python-cheetah
-    sudo apt install git python3-cryptography python3-openssl python3-lxml python3-pip
-    sudo git clone https://git.sickrage.ca/SiCKRAGE/sickrage /opt/sickrage
+    sudo apt install -y python3-cryptography python3-openssl python3-lxml python3-pip python3-setuptools python3-libxml2 python-libxslt1 
+    if [[ -d /opt/sickrage ]]; then
+        sudo git -C /opt/sickrage pull
+    else
+        sudo git clone https://git.sickrage.ca/SiCKRAGE/sickrage /opt/sickrage
+    fi
     sudo pip3 install --no-deps -r /opt/sickrage/requirements.txt
+    read -p "2"
     sudo python3 /opt/sickrage/SiCKRAGE.py
+    read -p "3"
     sudo addgroup --system sickrage
     sudo adduser --disabled-password --system --home /opt/sickrage --gecos "SiCKRAGE" --ingroup sickrage sickrage chown sickrage:sickrage -R /opt/sickrage cp -v /opt/sickrage/runscripts/init.systemd /etc/systemd/system/sickrage.service
     sudo chown root:root /etc/systemd/system/sickrage.service
