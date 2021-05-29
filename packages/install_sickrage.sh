10d2922df86d4546bc48c75143e463486f0a1832
Cor Jan van Zwol <cjvanzwol@gmail.com>
2021-05-29 09:08:11 +0200

    fix sickrage

--- packages/install_sickrage.sh
+++ packages/install_sickrage.sh
@@ -8,16 +8,16 @@ echo "Installing Sickrage"
 if [[ $OS == "OSMC" ]]; then
     #sudo apt-get install -qq git-core python python-cheetah
     sudo apt install git python3-cryptography python3-openssl python3-lxml python3-pip
-    git clone https://git.sickrage.ca/SiCKRAGE/sickrage /opt/sickrage
-    pip3 install --no-deps -r /opt/sickrage/requirements.txt
-    python3 /opt/sickrage/SiCKRAGE.py
-    addgroup --system sickrage
-    adduser --disabled-password --system --home /opt/sickrage --gecos "SiCKRAGE" --ingroup sickrage sickrage chown sickrage:sickrage -R /opt/sickrage cp -v /opt/sickrage/runscripts/init.systemd /etc/systemd/system/sickrage.service
-    chown root:root /etc/systemd/system/sickrage.service
-    chmod 644 /etc/systemd/system/sickrage.service
-    systemctl enable sickrage
-systemctl start sickrage
-systemctl status sickrage
+    sudo git clone https://git.sickrage.ca/SiCKRAGE/sickrage /opt/sickrage
+    sudo pip3 install --no-deps -r /opt/sickrage/requirements.txt
+    sudo python3 /opt/sickrage/SiCKRAGE.py
+    sudo addgroup --system sickrage
+    sudo adduser --disabled-password --system --home /opt/sickrage --gecos "SiCKRAGE" --ingroup sickrage sickrage chown sickrage:sickrage -R /opt/sickrage cp -v /opt/sickrage/runscripts/init.systemd /etc/systemd/system/sickrage.service
+    sudo chown root:root /etc/systemd/system/sickrage.service
+    sudo chmod 644 /etc/systemd/system/sickrage.service
+    sudo systemctl enable sickrage
+    sudo systemctl start sickrage
+    sudo systemctl status sickrage
 elif [[ $OS == "NAS" ]]; then
     local dir=/volume1/@appstore/sickbeard-custom
     if [[ -d $dir ]]; then
@@ -28,8 +28,3 @@ elif [[ $OS == "NAS" ]]; then
   fi
 fi
 echo "SICKRAGE DONE"
-ne now"
-fi
-RAGE DONE"
-ne now"
-fi
