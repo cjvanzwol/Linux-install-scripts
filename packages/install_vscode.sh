b6d62900f957037487f029fb898e3ad5b4f79492
Cor Jan van Zwol <cjvanzwol@gmail.com>
2021-06-01 21:34:36 +0200

    updates codile release canodate 10

--- packages/install_vscode.sh
+++ packages/install_vscode.sh
@@ -1,4 +1,4 @@
-#! /bin/nash
+#! /bin/bash
 # preload functions
 source $(find ~ -name Linux-install-scripts 2>/dev/null)/functions.sh
 [[ $FASE == "" ]] && FASE=vscode
@@ -6,7 +6,8 @@ source $(find ~ -name Linux-install-scripts 2>/dev/null)/functions.sh
 # Installing package
 if [[ $OS == "ChromeOS" ]]; then
     echo "Installing Codile"
-    get https://github.com/dimkr/codile/releases/download/0.0.1-862ea6d/codile_0.0.1_amd64.deb
+    #get https://github.com/dimkr/codile/releases/download/0.0.1-862ea6d/codile_0.0.1_amd64.deb
+    get https://github.com/dimkr/codile/releases/download/0.0.1-v10-amd64/codile_0.0.1_amd64.deb
     sudo ln -s /usr/bin/codile /usr/bin/vscode
 	INSTALLED_APP="Codile"
 else
