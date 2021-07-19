8581c33d2277e78760094c07a3c5c2afc67d7c17
Cor Jan van Zwol <cjvanzwol@gmail.com>
2021-07-19 13:24:26 +0000

    all

--- umbrel/format.sh
+++ umbrel/format.sh
@@ -7,11 +7,24 @@ fi
 
 rm /home/umbrel/*.gb
 lsblk | grep sd
-read -p "What drive? /dev/sd[last letter] " LETTER
-DRIVE=/dev/sd${LETTER}
+read -p "What drive? /dev/sd[last letter] " LETTRE
+DRIVE=/dev/sd${LETTRE}
 SUCCES=true
 read -p "Partition one drive or run test lager sizes? [1 for 1/other for test] " nxt
 read -p "How many GB? (start size when testing) " SIZE
+read -p "Resize to max after format? [y/n] " RESIZE
+read -p "Debug? [y/n] " DEBUG
+
+pause () {
+  [[ $DEBUG == "y" ]] && read -p "DEBUG" d
+}
+
+check_repair () {
+  sleep 1
+  umount ${DRIVE}1
+  sleep 1
+  fsck.ext4 -v ${DRIVE}1 -y && e2fsck -b 32768 ${DRIVE}1 -y
+}
 
 format () {
 echo "Trying ${SIZE}GB"
@@ -21,14 +34,25 @@ parted $DRIVE mklabel gpt
 parted $DRIVE mkpart primary 0GB ${SIZE}GB
 sleep 1
 mkfs.ext4 ${DRIVE}1
-fsck.ext4 -v /dev/sdb1 -y && e2fsck -b 32768 /dev/sdb1 -y
+pause
+check_repair
+pause
 mount ${DRIVE}1 /mnt/data || SUCCES=false
+pause
 if $SUCCES; then
   mkdir -p /mnt/data/umbrel
+  touch /mnt/data/umbel/.umbrel
   #source test.sh
-  source speed.sh $DRIVE
+  #source speed.sh $LETTRE
 fi
-SIZE=$(( $SIZE * 10 ))
+if [[ $RESIZE == "y" ]]; then
+  umount ${DRIVE}1
+  pause
+  parted ${DRIVE} resizepart 1 2000GB
+  pause
+  check_repair
+  SIZE=$(( $SIZE * 10 ))
+fi
 }
 
 while $SUCCES; do
@@ -39,4 +63,6 @@ done
 echo ""
 echo ""
 ls -hal ~
+echo ""
+ls -hal /mnt/data
 exit 0
